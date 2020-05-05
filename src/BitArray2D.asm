faeca210c709cba3f4b2c13c7b5839c094a1db5d
KadeR-jpg <kade.pitsch@student.montana.edu>
2020-05-04 23:12:52 -0600

    May 4/20 Commit PA2

--- src/BitArray2D.asm
+++ src/BitArray2D.asm
@@ -23,16 +23,6 @@ set_bit_elem:
         imul rdx, -1
         add rdx, rax
         mov rcx, 8
-        sub rcx, rdx
-        sub rcx, 1
-        mov rdx, 1
-        .LOOP:
-            sal rdx, 1
-            dec rcx
-            cmp rcx, 0
-            jg  .LOOP
-        or rdi, rdx
-        mov rax, rdi
         mov rsp, rbp        ; restore stack pointer to before we pushed parameters onto the stack
         pop rbp             ; remove rbp from the stack to restore rsp to initial value
         ret
@@ -47,28 +37,18 @@ get_bit_elem:
         ; rsi contains row width
         ; rdx contains row
         ; rcx contains col
-
        mov rax, rdx
-       imul rax, rsi
-       add rax, rcx
-       mov rcx, rax
-       sar rcx, 3
-       imul rdx, -1
-       add rdx, rax
-       mov rcx, 8
-       sub rcx, rdx
        sub rcx, 1
        mov rdx, 1
-       .loop1:
+       .LOOP:
            sal rdx, 1
            dec rcx
-           cmp rcx, 0
-           jg  .loop1
-           and rdx, rdi
-           mov rax, rdx
-           cmp rax, 1
-           setl al
-           movzx rax, al
+           test rcx, rcx
+           jg  .LOOP
+       and rdx, rdi
+       mov rax, rdx
+       cmp rax, 1
+       setl al
         mov rsp, rbp        ; restore stack pointer to before we pushed parameters onto the stack
         pop rbp             ; remove rbp from the stack to restore rsp to initial value
         ret                 ; return value in rax
